819a7c4cc43a77e575c2d0e47e493a6456d3fe1a
Asror Mirzoev <77021488+alliseeisgold@users.noreply.github.com>
2026-05-10 12:11:22 +0300

    Add: reverse-string - submission-2

---
+++ Data Structures & Algorithms/reverse-string/submission-2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int len = s.length-1;
+        for(int i = 0; i < s.length / 2; i++){
+            char tmp = s[i];
+            s[i] = s[len - i];
+            s[len - i] = tmp;
+        }
+    }
+}
