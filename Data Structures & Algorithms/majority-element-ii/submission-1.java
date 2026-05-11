1ef95b8af72e4e64182a8403b7b3a947f1b2067c
Asror Mirzoev <77021488+alliseeisgold@users.noreply.github.com>
2026-05-11 12:23:12 +0300

    Add: majority-element-ii - submission-1

---
+++ Data Structures & Algorithms/majority-element-ii/submission-1.java
@@ -0,0 +1,32 @@
+// kinda interesting solution)
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        Map<Integer, Integer> counter = new HashMap<>();
+        for (int num : nums) {
+            counter.put(num, counter.getOrDefault(num, 0) + 1);
+
+            if (counter.size() <= 2) {
+                continue;
+            }
+
+            Map<Integer, Integer> newCounter = new HashMap<>();
+            for (var entry : counter.entrySet()) {
+                if (entry.getValue() > 1) {
+                    newCounter.put(entry.getKey(), entry.getValue() - 1);
+                }
+            }
+            counter = newCounter;
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int key : counter.keySet()) {
+            int freq = 0;
+            for (int  num : nums) {
+                freq += (num == key) ? 1 : 0;
+            }
+            if (freq > nums.length / 3) {
+                result.add(key);
+            }
+        }
+        return result;
+    }
+}
